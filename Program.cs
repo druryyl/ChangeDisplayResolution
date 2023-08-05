15b240bd7e36b469d5ed22e15a58266a8d631797
Jude <drury_yl@yahoo.com>
2023-08-05 11:16:58 +0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,95 @@
+﻿using System.Runtime.InteropServices;
+
+namespace ChangeDisplayResolution
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create a DEVMODE structure to hold the display settings
+            DEVMODE devMode = new DEVMODE();
+            devMode.dmSize = (short)Marshal.SizeOf(typeof(DEVMODE));
+
+            // Get the current display settings
+            EnumDisplaySettings(null, -1, ref devMode);
+
+            if (args.Length == 0)
+            {
+                Console.WriteLine($"Current Resolution is: {devMode.dmPelsWidth}x{devMode.dmPelsHeight}");
+                Console.WriteLine("Use parameter -FHD, -HD+ or -HD to switch.");
+                return;
+            }
+
+            // Change the display resolution settings
+            switch (args[0].ToUpper())
+            {
+                case "FHD":
+                    devMode.dmPelsWidth = 1920;
+                    devMode.dmPelsHeight = 1080;
+                    break;
+                case "HD+":
+                    devMode.dmPelsWidth = 1600;
+                    devMode.dmPelsHeight = 900;
+                    break;
+                case "HD":
+                    devMode.dmPelsWidth = 1280;
+                    devMode.dmPelsHeight = 720;
+                    break;
+                default:
+                    Console.WriteLine($"Current Resolution is: {devMode.dmPelsWidth}x{devMode.dmPelsHeight}");
+                    Console.WriteLine("Use parameter -FHD, -HD+ or -HD to switch.");
+                    break;
+            }
+
+
+            // Apply the new settings
+            int result = ChangeDisplaySettings(ref devMode, 0);
+
+            if (result == 0)
+            {
+                Console.WriteLine("Display resolution changed successfully.");
+            }
+            else
+            {
+                Console.WriteLine("Failed to change display resolution.");
+            }
+        }
+
+        // Import the necessary Windows API functions
+        [DllImport("user32.dll")]
+        public static extern bool EnumDisplaySettings(string? deviceName, int modeNum, ref DEVMODE devMode);
+
+        [DllImport("user32.dll")]
+        public static extern int ChangeDisplaySettings(ref DEVMODE devMode, int flags);
+
+        // Define the DEVMODE structure
+        [StructLayout(LayoutKind.Sequential)]
+        public struct DEVMODE
+        {
+            [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 32)]
+            public string dmDeviceName;
+            public short dmSpecVersion;
+            public short dmDriverVersion;
+            public short dmSize;
+            public short dmDriverExtra;
+            public int dmFields;
+            public int dmPositionX;
+            public int dmPositionY;
+            public int dmDisplayOrientation;
+            public int dmDisplayFixedOutput;
+            public short dmColor;
+            public short dmDuplex;
+            public short dmYResolution;
+            public short dmTTOption;
+            public short dmCollate;
+            [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 32)]
+            public string dmFormName;
+            public short dmLogPixels;
+            public int dmBitsPerPel;
+            public int dmPelsWidth;
+            public int dmPelsHeight;
+            public int dmDisplayFlags;
+            public int dmDisplayFrequency;
+        }
+    }
+}
